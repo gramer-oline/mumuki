d1819089227f0ea78f715df1a717088cc2aed741
gramer-oline <santy07ramirezjoel@gmail.com>
2022-07-04 15:32:24 -0300

    update project base

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,12 +8,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Mumuki help',
   tagline: 'ejercicios resueltos de mumuki,Te ayudo a que no te traves en los ejercicios',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://gramer-oline.github.io',
   baseUrl: '/mumuki/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
+ 
+  
+  deploymentBranch:'gh-pages',
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'gramer-oline', // Usually your GitHub org/user name.
@@ -40,13 +43,7 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+       
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -78,12 +75,7 @@ const config = {
         links: [
           {
             title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
+           
           },
           {
             title: 'Community',
@@ -94,7 +86,7 @@ const config = {
               },
               {
                 label: 'Discord',
-               
+                href: 'https://twitter.com/',
               },
               {
                 label: 'Twitter',
@@ -105,10 +97,7 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+            
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/',
